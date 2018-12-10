2e81a0334b6512458bdfeeed11c4af493c5f0165
JoseUrbina <joseantonioug-09@hotmail.com>
2018-12-09 21:30:54 -0600

    Video 30

--- objects.js
+++ objects.js
@@ -3,13 +3,18 @@ function createPerson(name, age, active)
 	return {
 		name, 
 		age,
-		active
+		active,
+		showName(){
+			console.log(this.name);
+		}
 	};
 };
 
 let student1 = createPerson("Luis", 24, true);
+student1.showName();
 let student2 = createPerson("María",50, true);
 let student3 = createPerson("Pedroc", 24, true);
+let student4 = createPerson("Luis", 24, true);
 
 function getStudents(...students)
 {
@@ -20,6 +25,13 @@ console.log(getStudents(student1, student2, student3));
 
 // Video: 29: Metodos simples
 
+function saveData(validate)
+{
+	let message  = (validate)?"Save successefully":"Not save";
+
+	return message;
+}
+
 function createAnimal(name, place, age){
 	return {
 		name, 
@@ -27,9 +39,35 @@ function createAnimal(name, place, age){
 		age,
 		showData(){
 			return `${this.name} lives in ${place} and to be ${age} years old`;
+		},
+		saveData
+	}
+}
+
+
+
+let lion = createAnimal("Lion", "Africa", 15);
+console.log(lion.showData());
+console.log(lion.saveData(true));
+
+// VIdeo 30: Propiedades conmutadas
+
+let alias = "Product ";
+let nameProperty = "Active";
+
+function furniture(product, prize, active)
+{
+	return {
+		[alias + "Name"]: product,
+		[alias + "Prize"]: prize,
+		[nameProperty]: active,
+		showProductName(){
+			console.info(`*** ${this[alias + "Name"]} ***`);
 		}
 	}
 }
 
-let lion = createAnimal("Lion", "Africa", 15);
-console.log(lion.showData());
+var chair = furniture("Chair", 150.50, true);
+chair.showProductName();
+
+// VIdeo 31: Object.is
